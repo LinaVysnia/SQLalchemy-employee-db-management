922dff270b0fa154c6d865bf5554134104d8610d
Lina Vysnia <linavysnia@outlook.com>
2025-01-08 10:28:38 +0200

    asking the right questions

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from UI.mainUI import run_main_UI
+#do I need these?
 from classes.departmentClass import Department
 from classes.employeeClass import Employee
 from classes.projectClass import Project
