57d07ae315e9829d3587a7c0c1348fcd64ff3272
Lina Vysnia <linavysnia@outlook.com>
2025-01-08 10:28:49 +0200

    init in UI is not needed

--- UI/__init__.py
+++
@@ -1,9 +0,0 @@
-from modules.database import Base, engine
-from classes.employeeClass import Employee
-from classes.departmentClass import Department
-from classes.projectClass import Project
-
-# Base.metadata.create_all(bind=engine)
-
-#init file is run when something is imported from the folder that it's at. 
-#This current use is incorrect because every time something is imported, the tables are "created". It should be in another file and be run when the program starts
